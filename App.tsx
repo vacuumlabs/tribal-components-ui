5db7356cc0043c347af680fc7a392bd4ed78adaf
Juraj Flamik <juraj.flamik@vacuumlabs.com>
2020-03-14 00:22:54 +0100

    move expo-font from src

--- App.tsx
+++ App.tsx
@@ -1,14 +1,44 @@
 import {AppLoading, registerRootComponent} from 'expo'
+import * as Font from 'expo-font'
 import React, {useState} from 'react'
-import {init} from './src/utils/helpers'
 import Storybook from './storybook/index'
+
+export const expoInit = async () => {
+  await Font.loadAsync({
+    'Lato-Black': require('./assets/fonts/Lato-Black.ttf'),
+    'Lato-BlackItalic': require('./assets/fonts/Lato-BlackItalic.ttf'),
+    'Lato-Bold': require('./assets/fonts/Lato-Bold.ttf'),
+    'Lato-BoldItalic': require('./assets/fonts/Lato-BoldItalic.ttf'),
+    'Lato-Hairline': require('./assets/fonts/Lato-Hairline.ttf'),
+    'Lato-HairlineItalic': require('./assets/fonts/Lato-HairlineItalic.ttf'),
+    'Lato-Heavy': require('./assets/fonts/Lato-Heavy.ttf'),
+    'Lato-HeavyItalic': require('./assets/fonts/Lato-HeavyItalic.ttf'),
+    'Lato-Italic': require('./assets/fonts/Lato-Italic.ttf'),
+    'Lato-Light': require('./assets/fonts/Lato-Light.ttf'),
+    'Lato-LightItalic': require('./assets/fonts/Lato-LightItalic.ttf'),
+    'Lato-Medium': require('./assets/fonts/Lato-Medium.ttf'),
+    'Lato-MediumItalic': require('./assets/fonts/Lato-MediumItalic.ttf'),
+    'Lato-Regular': require('./assets/fonts/Lato-Regular.ttf'),
+    'Lato-Semibold': require('./assets/fonts/Lato-Semibold.ttf'),
+    'Lato-SemiboldItalic': require('./assets/fonts/Lato-SemiboldItalic.ttf'),
+    'Lato-Thin': require('./assets/fonts/Lato-Thin.ttf'),
+    'Lato-ThinItalic': require('./assets/fonts/Lato-ThinItalic.ttf'),
+    'PxGrotesk-Bold': require('./assets/fonts/PxGrotesk-Bold.otf'),
+    'PxGrotesk-Light': require('./assets/fonts/PxGrotesk-Light.otf'),
+    'PxGrotesk-Regular': require('./assets/fonts/PxGrotesk-Regular.otf'),
+  })
+}
 
 const App = () => {
   const [loading, setLoading] = useState(true)
 
   if (loading)
     return (
-      <AppLoading startAsync={init} onFinish={() => setLoading(false)} onError={console.error} />
+      <AppLoading
+        startAsync={expoInit}
+        onFinish={() => setLoading(false)}
+        onError={console.error}
+      />
     )
   return <Storybook />
 }
